78b0b8ff07f7c21c41e2730965bfc698eb7461da
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-08 21:19:39 +0200

    First example: getenv.html

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ about-php
 =========
 
 Small scripts about php.
+
+
+Testing
+=======
+
+The scripts were tested on Windows with [Uniform Server](http://www.uniformserver.com/).
+
---
+++ copy_to_uniformserver.bat
@@ -0,0 +1,8 @@
+@set uniform_server_dir=c:\tools\UniServerZ
+@set www_document_root=%uniform_server_dir%\www
+@set about_php_dir=%www_document_root%\about_php
+
+@mkdir %about_php_dir%
+
+@copy /y *.html       %about_php_dir%
+@copy /y .htaccess    %about_php_dir%
