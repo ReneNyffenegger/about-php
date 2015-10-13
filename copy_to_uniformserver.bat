35d0c55a1d2948db4778fb8125d4ab82c012eecc
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-10-13 18:58:57 +0200

    rmdir /s /q

--- copy_to_uniformserver.bat
+++ copy_to_uniformserver.bat
@@ -2,7 +2,8 @@
 @set www_document_root=%uniform_server_dir%\www
 @set about_php_dir=%www_document_root%\about_php
 
-@mkdir %about_php_dir%
+@rmdir /s /q %about_php_dir%
+@mkdir       %about_php_dir%
 
 @xcopy /e *.html       %about_php_dir%
 @xcopy /e *.php        %about_php_dir%
