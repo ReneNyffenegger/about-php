64cd3f895566d996c0fd3b244d8c5e78e423bbb2
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-10-27 11:23:10 +0100

    Use global env variable UNI_SERVER_Z_ROOT

--- copy_to_uniformserver.bat
+++ copy_to_uniformserver.bat
@@ -1,5 +1,4 @@
-@set uniform_server_dir=c:\tools\UniServerZ
-@set www_document_root=%uniform_server_dir%\www
+@set www_document_root=%UNI_SERVER_Z_ROOT%\www
 @set about_php_dir=%www_document_root%\about_php
 
 @mkdir %about_php_dir%
