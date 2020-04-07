9f972a71781120327b3c5e5a67ab8a51c1721f46
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-04-07 14:43:02 +0200

    + var_dump

---
+++ variable/var_dump.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+  <title>array_key_exists</title>
+</head>
+<body>
+
+<pre>
+<?php
+
+    $num = 42;
+    $txt ='Hello world';
+    $ary =['foo', 'bar', 'baz'];
+    $dct =['num' => 99, 'txt' => 'ciao'];
+
+   var_dump($num, $txt, $ary, $dct);
+
+?>
+</pre>
+
+</body>
+</html>
