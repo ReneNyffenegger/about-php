b40714e06518df94d4568e84a99ae76bcb140f85
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-10-08 23:06:41 +0200

    + wikimedia-SPARQL.php

---
+++ curl/wikimedia-SPARQL.php
@@ -0,0 +1,31 @@
+<?php
+
+    $query = '
+    select ?lifeExpectancy {
+       wd:Q39 wdt:P2250 ?lifeExpectancy .
+    }
+   ';
+
+   $headers = Array(
+    # "Content-Type: application/json",
+      "Accept: application/sparql-results+json",
+   );
+
+   $url         = 'https://query.wikidata.org/sparql';
+
+
+   $queryFields = ['query' => $query ];
+   $postFields  = http_build_query($queryFields);
+
+   $req         = curl_init();
+
+   curl_setopt($req, CURLOPT_URL           , $url);
+ # curl_setopt($req, CURLOPT_POST          , true);
+   curl_setopt($req, CURLOPT_POSTFIELDS    , $postFields);
+   curl_setopt($req, CURLOPT_USERAGENT     ,'Renes wikidata querier');
+   curl_setopt($req, CURLOPT_HTTPHEADER    , $headers);
+   curl_setopt($req, CURLOPT_RETURNTRANSFER, true); # Let curl_exec return the content rather than printing it to stdout?
+
+   $result = curl_exec($req);
+   print($result);
+?>
