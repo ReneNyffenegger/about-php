8733063e09676bcb4597ca915217dbebf315b7b6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-10-08 23:15:56 +0200

    more comments

--- curl/wikidata-SPARQL.php
+++ curl/wikidata-SPARQL.php
@@ -13,18 +13,17 @@
 
    $url         = 'https://query.wikidata.org/sparql';
 
-
    $queryFields = ['query' => $query ];
    $postFields  = http_build_query($queryFields);
 
    $req         = curl_init();
 
    curl_setopt($req, CURLOPT_URL           , $url);
- # curl_setopt($req, CURLOPT_POST          , true);
-   curl_setopt($req, CURLOPT_POSTFIELDS    , $postFields);
-   curl_setopt($req, CURLOPT_USERAGENT     ,'Renes wikidata querier');
+ # curl_setopt($req, CURLOPT_POST          , true);                    # 
+   curl_setopt($req, CURLOPT_POSTFIELDS    , $postFields);             # Implicitely set by CURLOPT_POSTFIELDS
+   curl_setopt($req, CURLOPT_USERAGENT     ,'Renes wikidata querier'); # Prevent HTTPS Status 403
    curl_setopt($req, CURLOPT_HTTPHEADER    , $headers);
-   curl_setopt($req, CURLOPT_RETURNTRANSFER, true); # Let curl_exec return the content rather than printing it to stdout?
+   curl_setopt($req, CURLOPT_RETURNTRANSFER, true);                    # Let curl_exec return the content rather than printing it to stdout?
 
    $result = curl_exec($req);
    print($result);
