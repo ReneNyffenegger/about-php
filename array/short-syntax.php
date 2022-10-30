0076dbc77412c4d895664f2f13726e254942e5df
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-10-30 09:39:20 +0100

    array: short syntax

---
+++ array/short-syntax.php
@@ -0,0 +1,13 @@
+<?
+
+$array = ['one', 'two', 'three'];
+
+$dict  = [ 'txt' => 'hello world',
+           'num' => 42 ];
+
+print("<pre><code>");
+print("array[ 1   ] = " . $array[ 1   ] . "<br>");
+print("dict ['num'] = " . $dict ['num'] . "<br>");
+print("</code></pre>");
+
+?>
